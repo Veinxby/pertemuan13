cab5368e20beefbc6fbdf511665a2366a8eb2ddd
Veinxby <mteguh0106@gmail.com>
2023-06-19 09:55:05 +0700

    test

--- proses_login.php
+++ proses_login.php
@@ -15,7 +15,7 @@ $cek = mysqli_num_rows($query);
 if ($cek > 0 ) {
     $_SESSION['username'] = $username;
     $_SESSION['status'] = 'login';
-    header ('Location:tampil_data.php?pesan=Login berhasil');
+    header ('Location:tampil_data.php');
 }else{
     header ('Location:login.php?pesan=login gagal');
 }
